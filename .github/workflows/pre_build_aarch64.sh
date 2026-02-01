d6b04b752bd1da48c33e57c039bbc089d0349a48
gcanat <gui.canat@gmail.com>
2026-02-01 23:52:02 +0100

    disable aarch64 for now

--- .github/workflows/pre_build_aarch64.sh
+++ .github/workflows/pre_build_aarch64.sh
@@ -20,9 +20,3 @@ apt install -y binutils-aarch64-linux-gnu clang gcc-aarch64-linux-gnu \
   multistrap pkg-config python3-pip rpcsvc-proto xz-utils
 
 pip install "maturin>=1.3,<2.0" patchelf
-
-# # download ffmpeg archive and extract it
-# curl -L "$FFMPEG_DOWNLOAD_URL" -o ffmpeg.tar.xz
-# mkdir -p $FFMPEG_DIR
-# tar -xf ffmpeg.tar.xz -C $FFMPEG_DIR --strip-components=1
-# export FFMPEG_DIR=$FFMPEG_DIR
